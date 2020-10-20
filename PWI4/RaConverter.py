7ff8548a2f9799370b7ecd09fbfbdb3d07aabd34
chri87d9 <christoffer143@gmail.com.com>
2020-10-20 13:45:43 +0200

    Added hms2deg converter

--- PWI4/RaConverter.py
+++ PWI4/RaConverter.py
@@ -37,3 +37,24 @@ def DecConverter_DecitoDD(Dec):
         Dec_dms = ("%s:%s:%s"%(deg,str(mnt)[0:2],sec))
         return Dec_dms
 
+
+def HMS2deg(ra='', dec=''):
+    RA, DEC, rs, ds = '', '', 1, 1
+    if dec:
+        D, M, S = [float(i) for i in dec.split(":")]
+        if str(D)[0] == '-':
+            ds, D = -1, abs(D)
+        deg = D + (M/60) + (S/3600)
+        DEC = '{0}'.format(deg*ds)
+
+    if ra:
+        H, M, S = [float(i) for i in ra.split(":")]
+        if str(H)[0] == '-':
+            rs, H = -1, abs(H)
+        deg = (H*15) + (M/4) + (S/240)
+        RA = '{0}'.format(deg*rs)
+
+    if ra and dec:
+        return (RA, DEC)
+    else:
+        return RA or DEC
